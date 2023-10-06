f3348033a87848f779ce2eb4190857ba41e37aae
Erik Tromp <erik_tromp@hotmail.com>
2023-10-06 20:35:41 +0200

    WiFi.ino: reduce TX power a bit in an attempt to have a more stable Wi-Fi connection (as distance between phone and ESP is very small, not much power is needed)

--- VanLiveConnect/Wifi.ino
+++ VanLiveConnect/Wifi.ino
@@ -77,6 +77,9 @@ const char* SetupWifi()
 {
     WiFi.hostname(GetHostname());
 
+    // Seems to help in decreasing the jitter on the VAN bus bit timings
+    wifi_set_sleep_type(NONE_SLEEP_T);
+
   #ifdef WIFI_AP_MODE
 
     static const char* wifiSsid = WIFI_SSID;
@@ -92,6 +95,10 @@ const char* SetupWifi()
 
     WiFi.mode(WIFI_AP);
     WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+
+    // See https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/generic-class.html :
+    //WiFi.setPhyMode(WIFI_PHY_MODE_11G);
+    WiFi.setOutputPower(17.5);
 
   #ifdef WIFI_PASSWORD
     WiFi.softAP(wifiSsid, WIFI_PASSWORD, WIFI_CHANNEL, WIFI_SSID_HIDDEN, 4);
@@ -111,20 +118,15 @@ const char* SetupWifi()
 
     WifiConfig();
 
-    // TODO - does this decrease the jitter on the bit timings?
-    wifi_set_sleep_type(NONE_SLEEP_T);
-
     WiFi.mode(WIFI_STA);  // Otherwise it may be in WIFI_AP_STA mode, broadcasting an SSID like AI_THINKER_XXXXXX
     WiFi.disconnect();  // After reset via HW button sometimes cannot seem to reconnect without this
     WiFi.persistent(false);
     WiFi.setAutoConnect(true);
 
-    WiFi.setPhyMode(WIFI_PHY_MODE_11N);
-    //WiFi.setOutputPower(20.5);  // TODO - default value is any different? Better?
+    // See https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/generic-class.html :
+    //WiFi.setPhyMode(WIFI_PHY_MODE_11N);
+    WiFi.setOutputPower(17.5);
 
-    // TODO - using Wi-Fi, unfortunately, has a detrimental effect on the packet CRC error rate. It will rise from
-    // around 0.006% up to 0.1% or more. Underlying cause is timing failures due to Wi-Fi causing varying interrupt
-    // latency. Not sure how to tackle this.
   #ifdef WIFI_PASSWORD
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   #else
@@ -168,8 +170,7 @@ void WifiCheckStatus()
         Serial.printf_P(PSTR("Connected to Wi-Fi SSID '%s'\n"), WIFI_SSID);
         Serial.printf_P(PSTR("Wi-Fi signal strength (RSSI): %ld dB\n"), WiFi.RSSI());
 
-        // Might decrease number of packet CRC errors in case the board was previously using Wi-Fi (Wi-Fi settings are
-        // persistent?)
+        // Seems to help in decreasing the jitter on the VAN bus bit timings
         wifi_set_sleep_type(NONE_SLEEP_T);
 
         Serial.print(F("Please surf to: http://"));
